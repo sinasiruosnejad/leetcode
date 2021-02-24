c77a3c37605f2f0e521a28191e636d12a13329c4
Sina <sina.sirousnejad@gmail.com>
2021-02-25 01:14:38 +0330

    Special Positions in a Binary Matrix

--- 1582.py
+++ 1582.py
@@ -1,8 +1,44 @@
-matrix=[[0,0,0,0,0],
-        [1,0,0,0,0],
-        [0,1,0,0,0],
-        [0,0,1,0,0],
-        [0,0,0,1,1]]
+def string_to_list(string):
+    x=string
+    result=[]
+    i=1
+    while x[i]!=']':
+        if x[i]!=',':
+            j=i
+            temp=''
+            while x[j]!=',':
+                if x[j]==']':
+                    break
+                temp+=x[j]
+                j+=1
+                i=j
+                result.append(int(temp))
+        else:
+            i+=1
+    return result
+
+
+
+IN=input()
+LIST=[]
+temp=''
+i=1
+while i<len(IN):
+    if IN[i]!=']':
+        temp+=IN[i]
+    elif IN[i]==']':
+        temp+=IN[i]
+        i+=1
+        LIST.append(temp)
+        temp=''
+    i+=1
+
+matrix=[]
+
+for i in range(len(LIST)):
+    matrix.append(string_to_list(LIST[i]))
+
+
 
 result=0
 x=0
@@ -21,3 +57,7 @@ for i in range(len(matrix)):
         result+=1
 
 print(result)
+
+
+#example
+# [[0,0,0,0,0],[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,1]]
